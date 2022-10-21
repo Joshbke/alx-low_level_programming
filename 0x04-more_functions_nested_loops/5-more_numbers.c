412537f4934134cd7091ab387780be60a7f21811
Yosef Belachew <yosefbelachew39@gmail.com>
2022-10-21 14:55:12 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,21 @@
 
 void more_numbers(void)
 {
-	int n, count;
+int n, count;
 
-	for (count = 0; count <= 9; count++)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-			if (n > 9)
-			{
-				_putchar((n / 10) + '0');
-			}
-		_putchar((n % 10) + '0');				}
-			_putchar('\n');
+for (count = 0; count <= 9; count++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n > 9)
+{
+_putchar((n / 10) + '0');
+}
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
+}
 				}
 				}
 
